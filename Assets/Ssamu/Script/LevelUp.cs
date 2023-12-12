d4b1e77e53ba9a2a71bec10173df38866172b4fd
Seokwon Heo <swheo92@gmail.com>
2023-12-12 20:49:47 +0900

    Add Random Item Popup

--- Assets/Ssamu/Script/LevelUp.cs
+++ Assets/Ssamu/Script/LevelUp.cs
@@ -13,6 +13,8 @@ public class LevelUp : MonoBehaviour
     }
 
     public void Show(){
+
+        Next();
         rect.localScale = Vector3.one;
         GameManager.instance.Stop();
     }
@@ -24,5 +26,36 @@ public class LevelUp : MonoBehaviour
 
     public void Select(int index){
         items[index].OnClick();
+    }
+
+    void Next(){
+        foreach(Item item in items){
+            item.gameObject.SetActive(false);
+        }
+
+        int[] ran = new int[3];
+        while(true){
+            ran[0] = Random.Range(0,items.Length);
+            ran[1] = Random.Range(0,items.Length);
+            ran[2] = Random.Range(0,items.Length);  
+
+            if(ran[0] != ran[1] && ran[1] != ran[2] && ran[0] != ran[2]){
+                break;
+            }
+                
+        }
+
+        for(int index = 0; index < ran.Length; index++){
+            Item ranItem = items[ran[index]];
+
+            if(ranItem.level == ranItem.data.damages.Length){
+                items[Random.Range(4,7)].gameObject.SetActive(true);
+            }else{
+                ranItem.gameObject.SetActive(true);
+            }
+        }
+
+
+
     }
 }
